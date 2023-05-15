729d433ec073fd10d106ee6899af4d09fee2919d
DenisMelnik777 <129406422+DenisMelnik777@users.noreply.github.com>
2023-05-15 16:10:21 +0300

    Add files via upload

---
+++ channel.py
@@ -0,0 +1,22 @@
+import os
+from distutils.command.build import build
+
+from helper.youtube_api_manual import api_key
+
+
+class Channel:
+    """Класс для ютуб-канала"""
+    api_key: str = os.getenv('YT_API_KEY')
+
+
+    def __init__(self, channel_id: str) -> None:
+        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        self.channel_id = channel_id
+
+    def print_info(self) -> None:
+        """Выводит в консоль информацию о канале."""
+        print(self.channel_id)
+
+
+    def get_service(self):
+        return build('youtube', 'v3', developerKey=api_key)
