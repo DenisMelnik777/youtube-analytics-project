15562a0aeb32d2e755d1291c863f7b65524125b1
DenisMelnik777 <129406422+DenisMelnik777@users.noreply.github.com>
2023-05-17 13:57:33 +0300

    Add files via upload

--- src/channel.py
+++ src/channel.py
@@ -1,19 +1,21 @@
-import os
-from distutils.command.build import build
+from googleapiclient.discovery import build
 
 
 class Channel:
     """Класс для ютуб-канала"""
-    api_key: str = os.getenv('YT_API_KEY')
+    api_key: str = "YT_API_KEY"
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        self.service = Channel.get_service()
+        self.channel = self.service.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        print(self.channel_id)
+        print(self.channel)
 
-
-def get_service():
-    return build('youtube', 'v3')
+    @classmethod
+    def get_service(cls):
+        return build('youtube', 'v3', developerKey=cls.api_key)
